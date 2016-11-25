0f42ab5321aca509817c75d10733e45e7ad67c31
olilen <olilenelou@gmail.com>
2016-11-25 15:17:28 +0100

    read server key and certificate only if https is used

--- eocatserver.js
+++ eocatserver.js
@@ -8,28 +8,8 @@ var products = require('./routes/catalogue');
 var bodyParser = require('body-parser');
 
 
-// start mongodb
-/*
-console.log(shell.pwd().stdout);
-if (shell.exec('mongod --config ./conf/mongod.conf &').code !== 0) {
-  console.log('Error: Failed to start mongodb');
-}
-*/
-/*
-var mongoProcess = child_process.spawn('mongod',['--config','./conf/mongod.conf'], {async:true});
-mongoProcess.stdout.on('data', function(data) {
-  console.log(data);
-});
-*/
 var app = express();
 
-var credentials = {
-    key: fs.readFileSync('./ssl/server.key'),
-    cert: fs.readFileSync('./ssl/server.crt'),
-    //ca: fs.readFileSync('./ssl/ca.crt'),
-    //requestCert: true,
-    rejectUnauthorized: false
-};
 
 var protocol = (process.argv[2])?process.argv[2]:'http';
 var port = (process.argv[3])?process.argv[3]:'3000';
@@ -47,6 +27,16 @@ app.put('/products/:id', products.updateProduct);
 app.delete('/products/:id', products.deleteProduct);
 
 if(protocol == 'https') {
+  try {
+    var credentials = {
+        key: fs.readFileSync('./ssl/server.key'),
+        cert: fs.readFileSync('./ssl/server.crt'),
+        rejectUnauthorized: false
+    };
+  }
+  catch (err) {
+    console.log("Could not read the server key or certificate in the ./ssl folder.");
+  }
   https.createServer(credentials, app).listen(port, function() {console.log("EOCat "+version+" is listening HTTPS on port "+port+"...");});
 } else {
   http.createServer(app).listen(port, function() {console.log("EOCat "+version+" is listening HTTP on port "+port+"...");});
