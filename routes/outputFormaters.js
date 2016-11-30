acc7d26d602efce436114f0d419f4ec71641fa77
olilen <olilenelou@gmail.com>
2016-11-30 15:28:16 +0100

    added missing attribute features[].type in search responses in owc format

--- routes/outputFormaters.js
+++ routes/outputFormaters.js
@@ -14,6 +14,7 @@ exports.owcFormat = function(item,model) {
   	delete newItem._id;
   	newItem.id = newItem.identifier;
   	delete newItem.identifier;
+		newItem.type = "Feature";
   	return newItem;
 
   } catch (err) {
