a1bd9aa6ca9d81113a436e6bdfc6ed7d9afa4bd9
olilen <olilenelou@gmail.com>
2017-07-26 15:27:08 +0200

    Improved procedure for generating SSL certificates for https

--- utilities/generate_ssl_keycert.sh
+++ utilities/generate_ssl_keycert.sh
@@ -2,12 +2,20 @@
 rm ssl/*
 rmdir ssl
 mkdir ssl
-pwgen 50 1 -s
-#openssl genrsa -des3 -out ssl/ca.key 1024
-#openssl req -new -key ssl/ca.key -out ssl/ca.csr
-#openssl x509 -req -days 365 -in ssl/ca.csr -out ssl/ca.crt -signkey ssl/ca.key
-openssl genrsa -des3 -out ssl/server.key 1024
-openssl req -new -key ssl/server.key -out ssl/server.csr
-cp ssl/server.key ssl/server.key.passphrase
-openssl rsa -in ssl/server.key.passphrase -out ssl/server.key
-openssl x509 -req -days 365 -in ssl/server.csr -signkey ssl/server.key -out ssl/server.crt
+
+
+# This generates a self-signed certificate
+
+#openssl genrsa -des3 -out ssl/server.key 1024
+#openssl req -new -key ssl/server.key -out ssl/server.csr
+#cp ssl/server.key ssl/server.key.passphrase
+#openssl rsa -in ssl/server.key.passphrase -out ssl/server.key
+#openssl x509 -req -days 365 -in ssl/server.csr -signkey ssl/server.key -out ssl/server.crt
+
+
+
+# This generates a non signed certificate
+
+openssl req -x509 -newkey rsa:2048 -keyout ssl/key.pem -out ssl/cert.pem -days 365
+# Following line can be commented if the passphrase option line is uncommented in eocatserver.js
+openssl rsa -in ssl/key.pem -out ssl/newkey.pem && mv ssl/newkey.pem ssl/key.pem
